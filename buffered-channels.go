91cda843abd7a5d078eeacab6d879e296efa7258
Chris Siannas <xrsia@otenet.gr>
2022-02-12 14:52:23 +0200

    Outline

--- buffered-channels.go
+++ buffered-channels.go
@@ -1,5 +1,11 @@
 package main
 
 func main() {
+	//make channel with 6 buffers
+
+	//spin a goroutine by which you send
+	//numbers 0-5 into the channel
+
+	//receive the contents of the buffered channel
 
 }
